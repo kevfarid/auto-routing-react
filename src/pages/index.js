79616fc9d2cbb0876fa5b786d1c661a2b3d81e93
Kevin Cruz <hi@kevfarid.dev>
2022-05-21 00:36:11 -0500

    feat(router): add auto-routing basic

---
+++ src/pages/index.js
@@ -0,0 +1,2 @@
+export { default as Index } from "../pages/Home";
+export { default as NotFound } from "../pages/NotFound";
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
-import { Home } from "../pages/Home";
+import * as Paths from "../pages";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" element={<Home />} />
+        {Object.keys(Paths).map((key) => {
+          const Element = Paths[key];
+
+          const keyToLowerCase = key.toLowerCase();
+
+          let path = keyToLowerCase;
+
+          if (keyToLowerCase === "home" || keyToLowerCase === "index") {
+            path = "/";
+          } else if (keyToLowerCase === "notfound") {
+            path = "*";
+          }
+
+          return <Route path={path} element={<Element />} key={key} />;
+        })}
       </Switch>
     </BrowserRouter>
   );
